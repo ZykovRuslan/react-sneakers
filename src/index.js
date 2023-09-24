01795e39d78ad6f5f630d67a42eae43b107c9ac6
Ruslan Zykov <r.v.zykov@rambler.ru>
2023-09-24 19:46:01 +0400

    feat: добавил адаптивность

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  // <React.StrictMode>
-  <Router>
-    <App />
-  </Router>,
-  // </React.StrictMode>
-  //!закоментил, потому что запросы к апи делает дважды
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
 )
